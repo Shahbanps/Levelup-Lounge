082bb737090dbeb6a7008b294b421b6872303dd3
Shahban ps <psshahban@gmail.com>
2023-04-26 01:52:03 +0530

    Admin store

---
+++ lib/Owner/add_item_store.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:levelup/User/feedback.dart';
+
+class AddItem extends StatefulWidget {
+  final String item_name;
+  final String item_price;
+  final String image_path;
+
+  AddItem(
+      {required this.item_name,
+      required this.item_price,
+      required this.image_path});
+
+  @override
+  _AddItemState createState() => _AddItemState();
+}
+
+class _AddItemState extends State<AddItem> {
+  final _formKey = GlobalKey<FormState>();
+  late String _name;
+  late String _email;
+  late String _address;
+
+  @override
+  void initState() {
+    super.initState();
+    _name = widget.item_name;
+    _email = widget.item_price;
+    _address = widget.image_path;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: false,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'Edit Profile',
+          style: GoogleFonts.bebasNeue(
+            fontSize: 30,
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      ),
+      body: Container(
+        color: Color.fromARGB(255, 0, 0, 0),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 90,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 26, 26, 26),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              padding: EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      initialValue: _name,
+                      style: GoogleFonts.bebasNeue(
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'Item Name',
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.yellow),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your item_name';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _name = value!;
+                      },
+                    ),
+                    TextFormField(
+                      initialValue: _email,
+                      style: GoogleFonts.bebasNeue(
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'Item Price',
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.yellow),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your item_price';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _email = value!;
+                      },
+                    ),
+                    TextFormField(
+                      initialValue: _address,
+                      style: GoogleFonts.bebasNeue(
+                        fontSize: 20,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      ),
+                      decoration: InputDecoration(
+                        labelText: 'Images',
+                        labelStyle: TextStyle(color: Colors.white),
+                        enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.yellow),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your image_path';
+                        }
+                        return null;
+                      },
+                      //somethingwrong
+                      onSaved: (value) {
+                        _address = value!;
+                      },
+                    ),
+                    SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          // Add code here to update the user's profile information.
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text(
+                        'Save',
+                        style: GoogleFonts.bebasNeue(
+                          fontSize: 20,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                      ),
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.yellow),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:levelup/Owner/add_item_store.dart';
 import 'package:levelup/login.dart';
 import 'package:levelup/registration.dart';
 
+import 'Owner/admin_store_dashboard.dart';
 import 'User/dashboard.dart';
 import 'User/feedback.dart';
 import 'User/profile_screen.dart';
@@ -47,34 +49,39 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Pages[currentIndex],
-      bottomNavigationBar: Container(
-        color: Colors.black,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
-          child: GNav(
-            onTabChange: (i) {
-              setState(() {
-                currentIndex = i;
-              });
-            },
-            backgroundColor: Colors.black,
-            color: Color.fromARGB(255, 255, 255, 255),
-            activeColor: Color.fromARGB(255, 255, 255, 255),
-            tabBackgroundColor: Color.fromARGB(255, 31, 31, 31),
-            padding: EdgeInsets.all(16),
-            gap: 8,
-            tabs: const [
-              GButton(
-                icon: Icons.home_filled,
-                text: 'Home',
-              ),
-              GButton(icon: Icons.store_rounded, text: 'Store'),
-              GButton(icon: Icons.feedback_rounded, text: 'Feedback'),
-              GButton(icon: Icons.person_2_rounded, text: 'Profile'),
-            ],
-          ),
-        ),
+      // body: Pages[currentIndex],
+      // bottomNavigationBar: Container(
+      //   color: Colors.black,
+      //   child: Padding(
+      //     padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+      //     child: GNav(
+      //       onTabChange: (i) {
+      //         setState(() {
+      //           currentIndex = i;
+      //         });
+      //       },
+      //       backgroundColor: Colors.black,
+      //       color: Color.fromARGB(255, 255, 255, 255),
+      //       activeColor: Color.fromARGB(255, 255, 255, 255),
+      //       tabBackgroundColor: Color.fromARGB(255, 31, 31, 31),
+      //       padding: EdgeInsets.all(16),
+      //       gap: 8,
+      //       tabs: const [
+      //         GButton(
+      //           icon: Icons.home_filled,
+      //           text: 'Home',
+      //         ),
+      //         GButton(icon: Icons.store_rounded, text: 'Store'),
+      //         GButton(icon: Icons.feedback_rounded, text: 'Feedback'),
+      //         GButton(icon: Icons.person_2_rounded, text: 'Profile'),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+      body: AddItem(
+        image_path: '',
+        item_name: '',
+        item_price: '',
       ),
     );
   }
